da741a129996e81d6c0052ece56b4b1ab8251ad5
sameer2831 <sameerb2831@gmail.com>
2025-05-26 13:05:41 -0400

    Add project files.

---
+++ FinancialManagementSystem/DAL/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinancialManagementSystem.Models;
+
+namespace FinancialManagementSystem.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
